6ad39ba3d08682c010b3fecb5cb089bd4f2c9679
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-12 14:53:55 +0100

    tmp

---
+++ Tests/UIDevice+ExtensionTests.swift
@@ -0,0 +1,13 @@
+//
+//  UIDevice+ExtensionTests.swift
+//  FTLinearActivityIndicator
+//
+//  Created by Roman Podymov on 12.02.23.
+//
+
+import FTLinearActivityIndicator
+import XCTest
+
+class UIDeviceExtensionTests: XCTestCase {
+    func test
+}
